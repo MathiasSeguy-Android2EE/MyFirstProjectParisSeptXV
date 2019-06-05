b24f32c59f9164c072c4dc85c1c2634d70884401
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 20:23:58 +0200

    First commit after migration of may 2019

--- app/src/main/java/com/android2ee/myfirstprojectparisseptxv/MainFragment.java
+++ app/src/main/java/com/android2ee/myfirstprojectparisseptxv/MainFragment.java
@@ -33,8 +33,8 @@ package com.android2ee.myfirstprojectparisseptxv;
 
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
